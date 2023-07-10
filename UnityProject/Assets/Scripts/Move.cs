fe697d84e29c84a2a0834da3915ac77b6a12d2f0
heechanson <heechanson1@gmail.com>
2023-07-10 15:59:51 +0900

    Move
    rowkfaksema z

---
+++ UnityProject/Assets/Scripts/Move.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;      // 캐릭터 움직임 스피드
+    public Vector2 movePoint; // 이동 위치 저장
+    public Camera mainCamera; // 메인 카메라
+
+    Vector3 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4.0f;
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, transform.forward, 15);
+            Debug.DrawRay(mousePosition, transform.forward * 10, Color.red, 0.3f);
+            if (hit.collider != null)
+            {
+                movePoint = hit.point;
+                Debug.Log(movePoint);
+                
+            }
+        }
+
+        MovePlayer();
+
+
+    }
+    void MovePlayer()
+    {
+        Vector2 thisUpdatePoint = (movePoint - new Vector2(transform.position.x, transform.position.y)).normalized;
+        gameObject.transform.position = Vector2.MoveTowards(transform.position, movePoint, speed*Time.deltaTime);
+    }
+}
