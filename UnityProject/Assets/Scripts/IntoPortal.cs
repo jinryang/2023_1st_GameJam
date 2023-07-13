d154c92be8f6f2558e13323c0c7a6ff517788c03
jinryang <84126292+jinryang@users.noreply.github.com>
2023-07-13 11:10:14 +0900

    Map

---
+++ UnityProject/Assets/Scripts/IntoPortal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class IntoPortal : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
